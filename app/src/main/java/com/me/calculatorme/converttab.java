2e0a7b8923d4dcb4232a3e1c38bdc8e531b48286
Phuong Nguyen Ngoc Uyen <uyenphuong1898@gmail.com>
2019-08-13 13:54:35 +0700

    initial commit

---
+++ app/src/main/java/com/me/calculatorme/converttab.java
@@ -0,0 +1,19 @@
+package com.me.calculatorme;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class converttab extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater.inflate(R.layout.converttab), container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/me/calculatorme/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.me.calculatorme.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.me.calculatorme.R;
+import com.me.calculatorme.calculatortab;
+import com.me.calculatorme.converttab;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                calculatortab tab1 = new calculatortab();
+                return tab1;
+            case 1:
+                converttab tab2 = new converttab();
+                return tab2;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+}
